d3b8aba4c257b0c16b8ca56b28640f59742c124c
vmaria <vmaria@combit.hu>
2023-01-03 08:23:59 +0100

    Change page title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { ToggleProvider } from '../providers/ToggleProvider'
+import  Head  from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <ToggleProvider><Component {...pageProps} /></ToggleProvider>
+  return <ToggleProvider>
+    <Head>
+      <title>K&V-Esküvő</title>
+      <meta property="og:title" content="Kata és Viktor közös esküvője" key="title" />
+    </Head>
+    <Component {...pageProps} />
+  </ToggleProvider>
 }
 
 export default MyApp
